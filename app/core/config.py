7016789ee1a7740a641d01d0d1f22ec436726490
valenarista <valuarista@gmail.com>
2026-03-04 21:13:30 -0300

    Add Docker support with Dockerfile and docker-compose, and create example environment file

--- app/core/config.py
+++ app/core/config.py
@@ -1,12 +1,11 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env")
     DATABASE_URL: str
     SECRET_KEY: str
     ALGORITHM: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int
 
-    class Config:
-        env_file = ".env"
 
 settings = Settings() # type: ignore
